6e598e51de57459fd3b5545f63d30d533aa8ba06
Nicholas Igunbor <nicholas.igunbor@decagon.dev>
2022-12-19 22:04:59 +0100

    created a pagination codeblock using react hooks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+/* eslint-disable no-unused-vars */
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import Post from './components/Post';
 import './App.css';
 
-function App() {
+
+const App = () => {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostPerPage] = useState(10);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
+      setPosts(res.data);
+      setLoading(false);
+    }
+
+    fetchPosts();
+  }, []);
+  
+  // Get current posts
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mb-5">
+    <h1 className='text-primary mb-3'>My Blog</h1>
+    <Post posts={currentPosts} loading={loading} />
     </div>
   );
 }
